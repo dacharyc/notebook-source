a328bac3b48b6cfefc63bed02b0c1dfeb1a2b016
dacharyc <dachary.carey@mongodb.com>
2024-10-23 10:30:51 -0400

    Print connection string to see what's up

--- go/example/manage-indexes/create-index-basic.go
+++ go/example/manage-indexes/create-index-basic.go
@@ -31,6 +31,7 @@ func ExampleCreateIndexBasic(t *testing.T) {
 	}
 	// Connect to server
 	uri := os.Getenv("ATLAS_CONNECTION_STRING")
+	fmt.Printf(uri)
 	if uri == "" {
 		log.Fatal("set your 'ATLAS_CONNECTION_STRING' environment variable.")
 	}
