34b15fbba0fdfba9a7e8ad4049453225d40e64d1
dacharyc <dachary.carey@mongodb.com>
2024-10-25 17:37:24 -0400

    Trigger test run

--- python/examples/queries/ann_basic.py
+++ python/examples/queries/ann_basic.py
@@ -1,3 +1,4 @@
+# :snippet-start: example
 import pymongo
 import os
 
@@ -282,10 +283,11 @@ def example():
     result = client["sample_mflix"]["embedded_movies"].aggregate(pipeline)
 
     # print results
-    docs = []
+    docs = [] # :remove:
     for i in result:
         print(i)
-        docs.append(i)
+        docs.append(i) # :remove:
 
     client.close()
-    return docs
+    return docs # :remove:
+# :snippet-end:
