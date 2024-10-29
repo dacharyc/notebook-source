ff1b0f109a5622a57f32be1607a9a957a42417b1
dacharyc <dachary.carey@mongodb.com>
2024-10-29 09:07:05 -0400

    Add JavaScript test suite

---
+++ javascript/examples/indexes/view-index.js
@@ -0,0 +1,22 @@
+import { MongoClient } from 'mongodb';
+
+export async function viewIndex() {
+    // connect to your Atlas deployment
+    const uri =  process.env.ATLAS_CONNECTION_STRING;
+    const client = new MongoClient(uri);
+    try {
+        const database = client.db("sample_mflix");
+        const collection = database.collection("embedded_movies");
+
+        // run the helper method
+        const result = await collection.listSearchIndexes("vector_index");
+        let indexes = [];
+        for await (const index of result) {
+            console.log(index);
+            indexes.push(index);
+        }
+        return indexes;
+    } finally {
+        await client.close();
+    }
+}
