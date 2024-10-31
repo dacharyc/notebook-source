67d1b9fc6687c6c27f4e9f673c75a42c0cf790e4
dacharyc <dachary.carey@mongodb.com>
2024-10-31 10:30:16 -0400

    Change class name to hopefully trigger test run

--- java/src/test/java/indexes/ManageIndexesTests.java
+++ java/src/test/java/indexes/ManageIndexesTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestManageIndexes {
+class ManageIndexesTests {
     @AfterEach
     void tearDown() {
         new DropIndex().main(new String[]{"Something"});
